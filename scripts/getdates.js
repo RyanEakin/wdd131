2e03b24c88255bd38cfaf10630857da9d1c3c2fc
Ryan Eakin <RyanEakin@protonmail.com>
2025-11-01 23:11:07 -0600

    seventh-commit-attempt-to-change-color-to-pass-contrast-test

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,11 +3,8 @@ const hoursBase = d.getHours();
 const minutesBase = d.getMinutes();
 const secondsBase = d.getSeconds();
 
-const hours = String(hoursBase).padStart(2,'0')
-const minutes = String(minutesBase).padStart(2,'0')
-const seconds = String(secondsBase).padStart(2,'0')
 
 const year = d.getFullYear();
 
 document.getElementById("currentYear").innerHTML = "&copy; " + year;
-document.getElementById("lastModified").innerHTML = "Last Modified: " + hours + ":" + minutes + ":" + seconds;
+document.getElementById("lastModified").innerHTML = document.lastModified;
