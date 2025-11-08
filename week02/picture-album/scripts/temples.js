3ce8438c54492b01f4a2b46797927ce97d15b5cf
Ryan Eakin <RyanEakin@protonmail.com>
2025-11-08 16:20:24 -0700

    week-02-assignment-02-temple-photo-album-commit

---
+++ week02/picture-album/scripts/temples.js
@@ -0,0 +1,26 @@
+// copied from getdates.js file
+const d = new Date();
+const hoursBase = d.getHours();
+const minutesBase = d.getMinutes();
+const secondsBase = d.getSeconds();
+
+
+const year = d.getFullYear();
+
+document.getElementById("currentYear").innerHTML = "&copy; " + year;
+document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified;
+// end of getdates.js file
+
+// copy of responsive-menu.js file
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+	navigation.classList.toggle("open");
+	hamButton.classList.toggle("open");
+});
+
+// end of copy
+
+//start of new code
