83cf98ac7e6d0d46f83e93a2d6e24ffe81057a1c
Ryan Eakin <RyanEakin@protonmail.com>
2025-11-17 23:39:32 -0700

    week03-country-page-seventh-commit

--- scripts/place.js
+++ scripts/place.js
@@ -1,23 +1,26 @@
 let thermometer = document.querySelector("#temp");
 let wind = document.querySelector("#wind")
+let chill = document.querySelector("#chill");
 
 let speed = 0;
 let temp = 0;
 let units = "none";
 
 
-function calculateWindChill(){
-    //currently empty, will fill later
+
+function calculateWindChill(temp, speed){
+    const windChill = 35.74 + (0.6215 * temp)- (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+    return windChill;
 }
 
 if (units === "metric") {
     if (temp <= 10 && speed > 4.8) {
-        //calculateWindChill();
+        calculateWindChill();
     }
 }
 else if (units === "imperial") {    
     if (temp <= 50 && speed > 3) {
-        //calculateWindChill();
+        calculateWindChill();
     }}
 else {
     console.log("N/A")
