4245e23ea0fc9b04fb1f2e93a78335895c1d2fb6
Ryan Eakin <RyanEakin@protonmail.com>
2025-12-11 16:36:38 -0700

    site-project-eight-commit

--- project/scripts/getdates.js
+++ project/scripts/getdates.js
@@ -10,21 +10,3 @@ document.getElementById("currentYear").innerHTML = "&copy; " + year;
 document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified;
 
 
-// start of form generation code
-
-function generateCompare() {
-    const selectList = document.querySelector('select')
-
-    products.forEach(productsList =>{
-        let newOption = document.createElement('option')
-        newOption.innerHTML = productsList.name;
-        newOption.value = productsList.id;
-
-        selectList.appendChild(newOption);
-})
-
-}
-
-window.generateCompare()
-
-// end of form generation code
